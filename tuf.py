d2858577de9d9470d3be31fdaa75827f31037c6e
Linux User <tsukigva@gmail.com>
2020-11-27 18:03:13 +0000

    yo

--- tuf.py
+++ tuf.py
@@ -1,18 +1,20 @@
 from collections import deque
 from sys import argv, exit
+from random import randrange
 import re
 
 full = {}
 prec = {}
 strs = {}
-tdpn = {}
 stck = {0:deque()}
-utks = {}
 ufun = {}
 sbuf = ""
 undefs = deque()
-sstk = 0  
+sstk = 0
+is_in_condition = deque([[False,False]])
 is_in_def = [False, "none"]
+conditionals_temp = []
+ignore = 0
 
 def prntsome():
     print(str(stck[sstk].pop()))
@@ -94,7 +96,7 @@ def s_mul():
 def s_div():
     num1 = stck[sstk].pop()
     num2 = stck[sstk].pop()
-    if num2 != 0:
+    if num2 != 0 and num1 != 0:
         stck[sstk].append(num1 / num2)
     else:
         print("zero division error")
@@ -107,17 +109,40 @@ def s_pow():
     stck[sstk].append(num1 ** num2)
     return True
 
+def conditional(word):
+    conditionals_temp.append([])
+    if "=" in word and int(re.search("\d+",word).group()) == stck[sstk][-1]:
+        is_in_condition.append([True,True])
+    else:
+        is_in_condition.append([True,False])
+    return True
+
+def checkEq(): 
+    global ignore # gambiarra merda, mudar isso um dia.
+    """                 .ROGER.
+    --- esse comando e estruturado como 'eq (1 2)'...
+    ... 1 e 2 sao colocados na stack e entao a igualdade...
+    ... entre eles e verificada. -- teclado sem acentos :p --
+                        .ROGER.                         """
+    if stck[sstk].pop() != stck[sstk].pop():
+        ignore += 1
+
+def skipp():
+    if stck[sstk]:
+
+
 _reserved = {"def":defun,"ASC":getasc,"LEN":getbuflen
         ,"emit":emitsome,".":prntsome,"CB":clbuf
         ,"new":new_stack,"select":chstack,"add":s_add
-        ,"sub":s_sub,"mul":s_mul,"div":s_div,"pow":s_pow}
+        ,"sub":s_sub,"mul":s_mul,"div":s_div,"pow":s_pow
+        ,"eq":checkEq}
 
 if len(argv) < 2:
-    print("too few arguments! Usage: python stuff.py -f somefile.tuf")
+    print("too few arguments! Usage: python tuf.py -f somefile.tuf")
     exit(1)
 
 elif "-f" not in argv:
-    print("no \"-f\" option supplied! use: python stuff.py -f somefile.tuf")
+    print("no \"-f\" option supplied! use: python tuf.py -f somefile.tuf")
     exit(1)
 
 with open(argv[argv.index("-f")+1]) as f:
@@ -158,27 +183,41 @@ if "-d" in argv or "--debug" in argv:
     print("\nstrings: " + str(strs))
     print("\nprecedence operations: " + str(prec))
     print("\nstacks: " + str(stck))
-    print("\ntime dependent operations: " + str(tdpn))
 
 def execute(word):
-    global is_in_def
-    if not is_in_def[0]:
-        if re.search("^\d+$",word) != None:
-            stck[sstk].append(int(word))
-            return True
-        else:
-            if word in _reserved:
-                _reserved[word]()
-            elif word in ufun:
-                for wd in ufun[word]:
-                    execute(wd)
-            elif "$" in word:
-                setbuf(word)
+    global is_in_def, is_in_condition, ignore
+    if ignore <= 0:
+        if is_in_condition[-1][0]:
+            if word == "]":
+                if is_in_condition[-1][1]:
+                    is_in_condition.pop()
+                    for wd in conditionals_temp[-1]: 
+                        execute(wd)
+                    conditionals_temp.pop()
+            else:
+                conditionals_temp[-1].append(word)
+            print(is_in_condition)
+        if not is_in_def[0] and not is_in_condition[-1][0]:
+            if re.search("^\d+$",word) != None:
+                stck[sstk].append(int(word))
+                return True
+            else:
+                if word in _reserved:
+                    _reserved[word]()
+                elif word in ufun:
+                    for wd in ufun[word]:
+                        execute(wd)
+                elif "$" in word:
+                    setbuf(word)
+                if "[" in word:
+                    conditional(word)
+        elif is_in_def[0]:
+            if word != "endef":
+                ufun[is_in_def[1]].append(word)
+            else:
+                is_in_def[0] = False
     else:
-        if word != "endef":
-            ufun[is_in_def[1]].append(word)
-        else:
-            is_in_def[0] = False
+        ignore -= 1
 
 for line in full.keys():
     for word in full[line]:
